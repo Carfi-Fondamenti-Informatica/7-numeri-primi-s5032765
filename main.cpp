a85a185aaf8480944efb8432ce331d14998e31b0
s5032765 <91558903+s5032765@users.noreply.github.com>
2021-12-01 21:28:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+using namespace std;
 
-int main(){
-  return 0;
+bool isPrime(int N, int C=2){
+    if(N==1)
+        return false;
+    if(N==2)
+        return true;
+    if(N%C==0)
+        return false;
+    if(C==N-1)
+        return true;
+    return isPrime(N,C+1);
+}
+
+int main() {
+    int N;
+    cin>>N;
+    if(isPrime(N))
+        cout<<"numero primo";
+    else
+        cout<<"numero non primo";
+
+    return 0;
 }
